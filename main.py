f763f7cc162ae6bf573af8f200a9009d7e753459
Lee094 <real.leearvib@gmail.com>
2015-08-31 19:19:36 +0800

    initial commit

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ class Thesis(ndb.Model):
 	section = ndb.StringProperty(indexed=True)
 	date = ndb.DateTimeProperty(auto_now_add=True)
 
+class Student(ndb.Model):
+	first_name = ndb.StringProperty(indexed=True)
+	last_name = ndb.StringProperty(indexed=True)
+	email = ndb.StringProperty(indexed=True)
+	number = ndb.StringProperty()
+	student_date = ndb.DateTimeProperty(auto_now_add=True)
+
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
 
@@ -44,7 +51,7 @@ class MainPageHandler(webapp2.RequestHandler):
         	template = JINJA_ENVIRONMENT.get_template('main.html')
         	self.response.write(template.render(template_data))
         else:
-        	self.redirect(users.create_login_url(self.request.uri))
+        	self.redirect('login')
 
 class ThesisCpE(webapp2.RequestHandler):
 	 
@@ -53,6 +60,8 @@ class ThesisCpE(webapp2.RequestHandler):
 		thesis_list = []
 
 		for thesis in thesis:
+			creator = thesis.app_user
+			created_by = ndb.Key('Student',creator)
 			thesis_list.append({
                     'id' : thesis.key.id(),
                     'Year' : thesis.year,
@@ -60,7 +69,9 @@ class ThesisCpE(webapp2.RequestHandler):
                     'Abstract' : thesis.abstract,
                     'Adviser' : thesis.adviser,
                     'Section' : thesis.section,
-                    'app_user' : thesis.app_user
+                    'app_user' : thesis.app_user,
+                    'creator_fName' : created_by.get().first_name,
+                    'creator_lName' : created_by.get().last_name
                 })
 
 		response = {
@@ -79,8 +90,11 @@ class ThesisCpE(webapp2.RequestHandler):
 		thesis.adviser = self.request.get('adviser')
 		thesis.section = self.request.get('section')
 		thesis.app_user = user.nickname()
-		thesis.app_user_id = user.user_id()
+		thesis.app_user = user.user_id()
 		thesis.put()
+
+		creator = thesis.app_user
+		created_by = ndb.Key('Student', creator)
 
 		self.response.headers['Content-Type'] = 'application/json'
 		response = {
@@ -92,10 +106,63 @@ class ThesisCpE(webapp2.RequestHandler):
 		            'abstract' : thesis.abstract,
 		            'adviser' : thesis.adviser,
 		            'section' : thesis.section,
-		            'app_user' : thesis.app_user
+		            'app_user' : thesis.app_user,
+		            'creator_fName' : created_by.get().first_name,
+                    'creator_lName' : created_by.get().last_name
 		    }
 		}
 		self.response.out.write(json.dumps(response))
+
+class Login(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		template_data = {
+			'login' : users.create_login_url(self.request.uri),
+			'register' : users.create_login_url(self.request.uri)
+		}
+		if user:
+			self.redirect('api/user')
+		else:
+			template = JINJA_ENVIRONMENT.get_template('login.html')
+			self.response.write(template.render(template_data))
+
+class Register(webapp2.RequestHandler):
+	def get(self):
+		user_login = users.get_current_user()
+
+		if user_login:
+			user_key = ndb.Key('Student', user_login.user_id())
+			user = user_key.get()
+			if user:
+				self.redirect('/')
+			else:
+				template = JINJA_ENVIRONMENT.get_template('register.html')
+				logout_url = users.create_logout_url('/login')
+				template_data = {
+					'logout_url' : logout_url
+				}
+				self.response.write(template.render(template_data))
+				template_data
+		else:
+			login_url = users.create_login_url('/api/user')
+			self.redirect(login_url)
+
+	def post(self):
+		user_login = users.get_current_user()
+		fName = self.request.get('first_name')
+		lName = self.request.get('last_name')
+		num = self.request.get('number')
+		email = user_login.email()
+		user_id = user_login.user_id()
+		user = Student(id=user_id, first_name=fName, last_name=lName, email=email, number=num)
+		user.put()
+
+		self.response.headers['Content-Type'] = 'application/json'
+		response = {
+			'result' : 'OK'
+		}
+		self.response.out.write(json.dumps(response))
+		self.redirect('/')
 
 class DeleteInfo(webapp2.RequestHandler):
     def get(self, studentId):
@@ -105,6 +172,8 @@ class DeleteInfo(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
 ('/api/thesis', ThesisCpE),
+('/login', Login),
+('/api/user', Register),
 ('/thesis/delete/(.*)', DeleteInfo),
 ('/home', MainPageHandler),
 ('/', MainPageHandler)
--- static/script.js
+++ static/script.js
@@ -16,8 +16,8 @@
 
 			// read response from server
 			if (response.status = 'OK') {
-				var thesis_info = thesis.year + ' - '  + thesis.thesis_title + ' created by: ' + thesis.app_user + ' <a href=\"/thesis/delete/'+thesis.id+'\"><button id=\"delete\" type=\"submit\">DELETE</button></a><hr> ';
-				$('.thesis-list').prepend('<li>' + thesis_info + '</li>')
+				var thesis_info = thesis.year + ' - '  + thesis.thesis_title + ' created by: ' + thesis.creator_fName + thesis.creator.lName + ' <a href=\"/thesis/delete/'+ thesis.id +'\"><button id=\"delete\" type=\"submit\">DELETE</button></a><hr> ';
+				$('.thesis-list').prepend('<li>' + thesis_info + '</li>');
 				$('input[type=text], [type=number]').val('');
 				$('select[name=year]').val('year');
 				$('select[name=section]').val('section');
@@ -41,8 +41,31 @@
 		});
 	}
 
+	function onRegFormSubmit(event) {
+		var data = $(event.target).serializeArray();
+		var user_data = {};
+
+		for (var i = 0; i < data.length; i++) {
+			var key = data[i].name;
+			var value = data[i].value;
+			user_data[key] = value;
+		}
+
+		var register_api = '/api/user';
+		$.post(register_api, user_data, function(response)
+		{
+			if (response.status = 'OK')
+			{
+				$(location).attr('href', 'http://ace-memento-9.appspot.com/');
+				return false;
+			}
+		})
+		return false;
+	}
+
 	$('form#form1').submit(onFormSubmit);
-	loadThesis();
+	loadThesis();	
+	$('form#registration').submit(onRegFormSubmit);
 
 	$(document).on('click', 'button#delete', function(){
 		$(this).closest('li').remove();
